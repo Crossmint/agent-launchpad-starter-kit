18fff9714ad59d56d422ed89538ed06c99aaf922
jmderby <jonathan.temp@paella.dev>
2025-02-05 19:26:50 -0800

    Elizaos: Add Agent

---
+++ agent-tee-phala/image/agent/elizaos/index.ts
@@ -0,0 +1,22 @@
+import type { Plugin } from "@elizaos/core";
+import { getOnChainActions } from "./actions";
+import { getWalletClient, getWalletProvider } from "./wallet";
+
+async function createGoatPlugin(getSetting: (key: string) => string | undefined): Promise<Plugin> {
+    const walletClient = getWalletClient(getSetting);
+    if (!walletClient) {
+        throw new Error("Wallet client not found");
+    }
+    const actions = await getOnChainActions(walletClient);
+
+    return {
+        name: "[GOAT] Onchain Actions",
+        description: "Mode integration plugin",
+        providers: [getWalletProvider(walletClient)],
+        evaluators: [],
+        services: [],
+        actions: actions,
+    };
+}
+
+export default createGoatPlugin;

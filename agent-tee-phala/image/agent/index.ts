a6e418d4a20478071d077c257ac6882c07a3274d
Jonathan <jonathan.temp@paella.dev>
2025-02-27 15:10:39 -0800

    Agent: Add vercel ai (#19)
    * Agent: Add vercel ai
    
    * feedback

--- agent-tee-phala/image/agent/index.ts
+++ agent-tee-phala/image/agent/index.ts
@@ -1,19 +1,9 @@
-import type { Character } from "@elizaos/core";
-import createElizaGoatPlugin from "./elizaos";
+import startVercelAiAgent from "./vercel-ai";
 
 export default async function startAgent() {
-    const plugins = await createElizaGoatPlugin();
-    // @ts-ignore TODO: Finish character and deploy the agent here...
-    const character: Character = {
-        name: "Rufus Agent",
-        adjectives: ["smart", "helpful", "friendly"],
-        lore: ["You are a helpful assistant that can help with a variety of tasks."],
-        bio: "You are a helpful assistant that can help with a variety of tasks.",
-        plugins: [plugins],
-        clients: [],
-        // services: [],
-    };
-    console.log("Skipping agent deployment...");
+    console.log("Starting agent...");
+    await startVercelAiAgent();
+    console.log("Agent started");
 }
 
 startAgent();
